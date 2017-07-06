4e18b3ab48946bc69890d99ec8bac44162a9e540
hgsongra <hgsongra@ymail.com>
2017-07-06 23:10:18 +0530

    Build API

---
+++ app/controllers/api/v1/state_transactions_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::StateTransactionsController < Api::V1::BaseController
+
+	before_action :set_state_transaction, only: [:update, :destroy]
+
+	def index
+		json_response(StateTransaction.all)
+	end
+
+	def create
+		state_transaction = StateTransaction.create!(transaction_param)
+		json_response(state_transaction, :created)
+	end
+
+	def update
+		@state_transaction.update(transaction_param)		
+		json_response(@state_transaction, :ok)
+	end
+
+	def destroy
+		@state_transaction.destroy!
+		head :no_content
+	end
+
+	private
+
+	def set_state_transaction
+		@state_transaction = StateTransaction.find(params[:id])
+	end
+
+	def transaction_param 
+		params.require(:state_transaction).permit(:street, :city, :zip, :state, :beds, :baths, :sq_feet, :prop_type, :sale_date, :price, :latitude, :longitude)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  
+  root 'state_transactions#index'
+  
   resources :state_transactions, only: :index do
   	post :import, on: :collection
   end
 
-  root 'state_transactions#index'
+  namespace :api, defaults: { format: :json } do
+  	namespace :v1 do
+  		resources :state_transactions, except: [:new, :edit, :show]
+  	end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
